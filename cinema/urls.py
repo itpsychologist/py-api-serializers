367f75c2421322ee99fafffc2e6ae96c472365e9
VikTOR <dev.psychologist@gmail.com>
2025-08-07 14:03:00 +0300

    First commit

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,25 @@
-# write urls here
+from rest_framework import routers
+from django.urls import path, include
+from cinema.views import (
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieViewSet,
+    GenreViewSet,
+    MovieSessionViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register(r"movies", MovieViewSet, basename="movie")
+router.register(r"cinema_halls", CinemaHallViewSet, basename="cinemahall")
+router.register(r"actors", ActorViewSet, basename="actor")
+router.register(r"genres", GenreViewSet, basename="genre")
+router.register(
+    r"movie_sessions",
+    MovieSessionViewSet,
+    basename="moviesession")
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+
+app_name = "cinema"
